43815f908b5863932d63789cc82ac87e3e7c06a6
Areln <andaaron419@hotmail.com>
2025-03-18 16:53:35 -0400

    update controls and manifest

--- inject/injected.js
+++ inject/injected.js
@@ -1,25 +1,103 @@
-function teleportToCoin(){
-    worldConfig.myCat.setPosition(worldConfig.rat.getPosition().x, worldConfig.rat.getPosition().y+1, worldConfig.rat.getPosition().z)
+var currentSpeed = 0.05;
+var showHideToggle = true;
+
+var zoomiesLabelElement = document.getElementById("zoomiesLabel");
+
+var chungusModeToggle = true;
+var defaultCatScale = worldConfig.myCat.catModel.children[0].scale;
+
+document.addEventListener("keydown", function (event) {
+    if (event.code === "Space") {
+        flyUp();
+    }
+    if (event.code === "ShiftLeft") {
+        flyDown();
+    }
+    if (event.key === "q") {
+        superSpeedUp();
+    }
+    if (event.key === "e") {
+        superSpeedDown();
+    }
+});
+
+function teleportToCoin() {
+    worldConfig.myCat.setPosition(
+        worldConfig.rat.getPosition().x,
+        worldConfig.rat.getPosition().y,
+        worldConfig.rat.getPosition().z
+    );
 }
 
-function becomeRat(){
-    worldConfig.myCat = worldConfig.rat
+function becomeRat() {
+    worldConfig.myCat = worldConfig.rat;
 }
 
-function hideCats(){
-    worldConfig.otherCats.forEach(x => x.catModel.visible = false)
+function toggleCatVis() {
+    showHideToggle = !showHideToggle;
+    worldConfig.otherCats.forEach((x) => (x.catModel.visible = showHideToggle));
 }
 
-function showCats(){
-    worldConfig.otherCats.forEach(x => x.catModel.visible = true)
-}
-
-function superSpeedDown(){
+function superSpeedDown() {
+    currentSpeed = currentSpeed - 0.05;
+    zoomiesLabelElement.innerHTML = "Zoomies (" + currentSpeed + "):";
     worldConfig.maxCatVelocity = worldConfig.maxCatVelocity - 0.05;
-    worldConfig.catRotationSpeed = worldConfig.catRotationSpeed - 0.05;
+    worldConfig.myCat.rockSpeed = worldConfig.myCat.rockSpeed - 0.25;
+    //worldConfig.catRotationSpeed = worldConfig.catRotationSpeed - 0.05;
 }
 
-function superSpeedUp(){
+function superSpeedUp() {
+    currentSpeed = currentSpeed + 0.05;
+    zoomiesLabelElement.innerHTML = "Zoomies (" + currentSpeed + "):";
     worldConfig.maxCatVelocity = worldConfig.maxCatVelocity + 0.05;
-    worldConfig.catRotationSpeed = worldConfig.catRotationSpeed + 0.05;
+    worldConfig.myCat.rockSpeed = worldConfig.myCat.rockSpeed + 0.25;
+    //worldConfig.catRotationSpeed = worldConfig.catRotationSpeed + 0.05;
+}
+
+function flyUp() {
+    worldConfig.myCat.setPosition(
+        worldConfig.myCat.getPosition().x,
+        worldConfig.myCat.getPosition().y + 1,
+        worldConfig.myCat.getPosition().z
+    );
+}
+
+function flyDown() {
+    worldConfig.myCat.setPosition(
+        worldConfig.myCat.getPosition().x,
+        worldConfig.myCat.getPosition().y - 1,
+        worldConfig.myCat.getPosition().z
+    );
+}
+
+function chungusMode() {
+    if (chungusModeToggle) {
+        setCatModelScale(worldConfig.myCat.catModel.children[0], 1, 1, 1);
+
+        worldConfig.otherCats.forEach((x) =>
+            setCatModelScale(x.catModel.children[0], 1, 1, 1)
+        );
+
+        setCatModelScale(worldConfig.rat.catModel.children[0], 100, 100, 100);
+        worldConfig.cameraDistance = 20; // Setting this too high causes the camera to bug out.
+    } else {
+        setCatModelScale(worldConfig.myCat.catModel.children[0], 0.1, 0.1, 0.1);
+
+        worldConfig.otherCats.forEach((x) =>
+            setCatModelScale(x.catModel.children[0], 0.1, 0.1, 0.1)
+        );
+
+        setCatModelScale(worldConfig.rat.catModel.children[0], 1, 1, 1);
+    }
+    chungusModeToggle = !chungusModeToggle;
+}
+
+function setCatModelScale(catModelChild, x, y, z) {
+    catModelChild.scale.x = x;
+    catModelChild.scale.y = y;
+    catModelChild.scale.z = z;
+}
+
+function newSpawnsRat() {
+    worldConfig.catObject = worldConfig.ratObject;
 }
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 
+// if (window.location.hostname.includes("https://maxwellcat.world/")) {
+//     injectUI();
+// }
+
 
 document.getElementById("injectHtml").addEventListener("click", function () {
     injectUI();
@@ -7,13 +11,11 @@ document.getElementById("injectHtml").addEventListener("click", function () {
 function injectUI() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         if (tabs.length === 0) return;
-
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             function: () => {
-                (async function() {
-                    console.log('Helloo');
-                    const response = await fetch(chrome.runtime.getURL("inject/injected.html"));
+                (async function () {
+                    const response = await fetch(chrome.runtime.getURL("ui/injected.html"));
                     const html = await response.text();
 
                     let container = document.getElementById("zoomControls");
@@ -23,7 +25,7 @@ function injectUI() {
                         container.append(controls)
 
                         const script = document.createElement("script");
-                        script.src = chrome.runtime.getURL("inject/injected.js");
+                        script.src = chrome.runtime.getURL("injected.js");
                         document.documentElement.appendChild(script);
                     }
                 })();
